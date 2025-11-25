e1358ae13a9f1d9440d637e41b9605a27f5f81bf
arthsonani <arthsonani@gmail.com>
2025-11-25 23:30:25 +0530

    defiend initial state

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,8 +11,10 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 import { useActionState } from 'react';
+import { createInvoice, State } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
 
   const createInvoiceAction = async (formData: FormData) => {
